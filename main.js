b13368c476d80877d0a377783023f73c3125ff65
frankillerfca-ux <frankiller.fca@gmail.com>
2026-01-13 21:14:14 -0500

    Fetch random Chuck Norris joke using axios

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-console.log("Hola desde Canadá! Este es mi primer actor funcionando.");
+const axios = require('axios');
+
+async function main() {
+    try {
+        const response = await axios.get('https://api.chucknorris.io/jokes/random');
+        console.log("Broma de Chuck Norris:");
+        console.log(response.data.value);
+    } catch (error) {
+        console.log("Error al obtener datos:", error.message);
+    }
+}
+
+main();
